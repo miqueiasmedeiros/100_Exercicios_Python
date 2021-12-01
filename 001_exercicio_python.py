5d9b84ae4750b8ffa3dc17fb4225684462d58641
Miqueias <miqueiascompany@gmail.com>
2021-11-30 23:07:25 -0300

    Exercicio 1 em Python

--- 001_exercicio_python.py
+++ 001_exercicio_python.py
@@ -1,15 +1,15 @@
-#Crie um script python que leia o nome de alguém e mostre uma mensagem de boas vindas.
+# Crie um script python que leia o nome de alguém e mostre uma mensagem de boas vindas.
 nome = input('Digite seu nome: ')
 print("É um prazer em te conhecer {}!" .format(nome))
 
-#Crie um script em python que leia a data de nascimento e mostra uma mensagem com a data formatada
+# Crie um script em python que leia a data de nascimento e mostra uma mensagem com a data formatada
 print('Digite sua data de nascimento: ')
 dia = input('Dia = ')
 mes = input('Mes = ')
 ano = input('Ano = ')
 print('Você nasceu no dia {} de {} de {}. Correto?'.format(dia, mes, ano))
 
-#Crie um script python que leia dois numeros e mostre a soma entre eles.
+# Crie um script python que leia dois numeros e mostre a soma entre eles.
 a1 = int(input('Digite o primeiro número: '))
 b2 = int(input('Digite o segundo número: '))
 s = a1+b2
