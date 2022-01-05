4d8353b609b25f2c96349ee18fe8009ad5a7dcbe
Miquéias Medeiros <miqueiascompany@gmail.com>
2022-01-04 21:19:08 -0300

    CALCULA GASTO DE TINTA

--- 009_exercicio_python.py
+++ 009_exercicio_python.py
@@ -7,4 +7,4 @@ altura = float(input('Digite a altura: '))
 largura = float(input('Digite a largura: '))
 area = altura*largura
 tinta = area/2
-print('Será necessário para pintar uma área de {:.2f}m², {:.2f} litros de tinta.' .format(area, tinta))
+print('Será necessário para pintar uma área de {:.3f}m², {:.2f} litros de tinta.' .format(area, tinta))
