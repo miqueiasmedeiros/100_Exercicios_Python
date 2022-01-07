6719513cb6a4230b23729cf7c604bd5b2a476b93
Miquéias Medeiros <miqueiascompany@gmail.com>
2022-01-07 06:22:19 -0300

    importando bibliotecas

---
+++ aula_modulos.py
@@ -0,0 +1,9 @@
+'''import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {:.2f}'.format(num, raiz))'''
+
+from math import sqrt
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
